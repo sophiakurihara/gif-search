f9bebb84583695faeef8940aaf47850fa8a5fcd9
sophiakurihara <sophia.sudiacal@gmail.com>
2020-12-31 11:49:20 -0600

    Search Form refactor. Added no results page and loading message

--- src/App.js
+++ src/App.js
@@ -9,29 +9,45 @@ export default class App extends Component {
   constructor() {
     super();
     this.state = {
-        gifs: []
+        gifs: [],
+        loading: true
     }
   } 
 
-  componentDidMount() {
-      axios.get('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
-          //Axios automatically parses response into JSON
-          .then(response => {
-              this.setState({ gifs: response.data.data})
-          })
-          .catch(error => {
-              console.log('Error fetching and parsing data', error);
-          });
-      // fetch('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
-      //     .then((response) => response.json())
-      //     .then((responseData) => {
-      //         this.setState({ gifs: responseData.data})
-      //     })
-      //     .catch(error => {
-      //         console.log('Error fetching and parsing data', error);
-      //     });
-  }
+    componentDidMount() {
+        // axios.get('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+        //   //Axios automatically parses response into JSON
+        //     .then(response => {
+        //         this.setState({ gifs: response.data.data})
+        //     })
+        //     .catch(error => {
+        //          console.log('Error fetching and parsing data', error);
+        //      });
 
+        // fetch('http://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC')
+        //     .then((response) => response.json())
+        //     .then((responseData) => {
+        //         this.setState({ gifs: responseData.data})
+        //     })
+        //     .catch(error => {
+        //         console.log('Error fetching and parsing data', error);
+        //     });
+        this.performSearch();
+    }
+
+    performSearch = (query = 'cats') => {
+        axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC`)
+        //Axios automatically parses response into JSON
+            .then(response => {
+                this.setState({
+                    gifs: response.data.data,
+                    loading: false
+                })
+            })
+            .catch(error => {
+                console.log('Error fetching and parsing data', error);
+            });
+    }
     render() {
     console.log(this.state.gifs);
     return (
@@ -39,11 +55,15 @@ export default class App extends Component {
         <div className="main-header">
           <div className="inner">
             <h1 className="main-title">GifSearch</h1>
-            <SearchForm />      
+            <SearchForm onSearch={this.performSearch}/>
           </div>   
         </div>    
         <div className="main-content">
-          <GifList />
+            {
+                (this.state.loading)
+                ? <p>Loading...</p>
+                : <GifList data={this.state.gifs} />
+            }
         </div>
       </div>
     );
