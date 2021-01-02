ea1e7dd0ec900691fd9ea37af2d5245b75da2169
Sophia <sophia.sudiacal@gmail.com>
2021-01-01 23:03:50 -0600

    refactored app to use Hooks

--- src/Components/App.js
+++ src/Components/App.js
@@ -8,13 +8,16 @@ import GifList from './GifList';
 function App() {
     const [data, setData] = useState([]);
     const [query, setQuery] = useState('cats');
+    const [isLoading, setIsLoading] = useState(true);
 
     //update the query state
     const performSearch = (value) => setQuery(value);
+
     useEffect(() => {
         axios(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC`)
             .then(response => setData(response.data.data))
             .catch(error => console.log('Error fetching and parsing data', error))
+            .finally( () => setIsLoading(false));
     }, [query]);
 
     return (
@@ -26,7 +29,11 @@ function App() {
           </div>
         </div>
         <div className="main-content">
-            <GifList data={data} />
+            {
+                isLoading
+                ? <p>Loading...</p>
+                : <GifList data={data} />
+            }
         </div>
       </>
     );
